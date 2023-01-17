c740c5e7eb38208da5e110a2b5646a970957694f
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-01-17 22:57:33 +0100

    chore: add tailwindcss autoprefixer postcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
 import Habit from './Habit'
+import './styles/global.css'
 
 function App() {
   return (
-    <>
+    <div>
       <Habit completed={1} />
       <Habit completed={10} />
       <Habit completed={20} />
       <Habit completed={30} />
-    </>
+    </div>
   )
 }
 
--- web/src/Habit/index.tsx
+++ web/src/Habit/index.tsx
@@ -5,7 +5,11 @@ type HabitProps = {
 }
 
 function Habit({ completed }: HabitProps) {
-  return <p>Habits {completed}</p>
+  return (
+    <div className='bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center flex items-center justify-center'>
+      Habits {completed}
+    </div>
+  )
 }
 
 export default Habit
