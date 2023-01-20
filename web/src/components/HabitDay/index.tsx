31abf9661f50d222a4d34569db0395aebc6654c8
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-01-20 23:52:13 +0100

    refactor: update component structure and styles

---
+++ web/src/components/HabitDay/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// type HabitProps = {
+//   completed: number
+// }
+
+function HabitDay() {
+  return (
+    // <div className='bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center flex items-center justify-center'>
+    //   Habits {completed}
+    // </div>
+    <div className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg' />
+  )
+}
+
+export default HabitDay
