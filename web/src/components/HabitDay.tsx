4988a5b640fb7b1454ba39f3aa673d995a835655
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-01-24 22:57:50 +0100

    refactor: update styles and layout

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
-// type HabitProps = {
-//   completed: number
-// }
+import * as Popover from '@radix-ui/react-popover'
+import ProgressBar from './ProgressBar'
 
 function HabitDay() {
   return (
-    // <div className='bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center flex items-center justify-center'>
-    //   Habits {completed}
-    // </div>
-    <div className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg' />
+    <Popover.Root>
+      <Popover.Trigger className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg' />
+      <Popover.Portal>
+        <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+          <span className='font-semibold text-zinc-400'>Segunda-feira</span>
+          <span className='mt-1 font-extrabold leading-tight text-3xl'>
+            17/01
+          </span>
+          <ProgressBar progress={70} />
+          <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
 
