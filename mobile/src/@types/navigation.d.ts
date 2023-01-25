d159d5d08c0d599b6e70be3bfb0fd8bd24a9de11
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-01-25 22:50:52 +0100

    refactor: update types

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,9 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: undefined
+      Habit: { date: string }
+      CreateHabit: undefined
+    }
+  }
+}
