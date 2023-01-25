c699fb98f3a95741efa5abbf33c2f75a44028b24
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-01-25 23:01:20 +0100

    chore: add new component textInput

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -3,12 +3,15 @@ import { TouchableOpacity, View, Text } from 'react-native'
 import Logo from '../assets/logo.svg'
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
+import { useNavigation } from '@react-navigation/native'
 
 const Header = () => {
+  const { navigate } = useNavigation()
   return (
     <View className='w-full flex-row items-center justify-between'>
       <Logo />
       <TouchableOpacity
+        onPress={() => navigate('CreateHabit')}
         activeOpacity={0.7}
         className='flex-row h-11 px-4 border border-violet-500 rounded-lg items-center '
       >
--- mobile/src/screens/CreateHabit.tsx
+++ mobile/src/screens/CreateHabit.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, View } from 'react-native'
+import { ScrollView, View, Text, TextInput } from 'react-native'
 import React from 'react'
 import { BackButton } from '../components'
 
@@ -7,6 +7,13 @@ const CreateHabit = () => {
     <View className='flex-1 bg-background px-8 pt-16'>
       <ScrollView showsVerticalScrollIndicator={false}>
         <BackButton />
+        <Text className='mt-6 text-white font-extrabold text-3xl'>
+          Criar Hábito
+        </Text>
+        <Text className='mt-6 text-white font-semibold text-base'>
+          Qual seu comprometimento?
+        </Text>
+        <TextInput className='h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600' />
       </ScrollView>
     </View>
   )
