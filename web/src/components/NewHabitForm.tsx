78a2cacf70c5609e2c2f9995fc33fe98398541d5
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-01-26 23:28:57 +0100

    chore: get data from form

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,7 +1,8 @@
 import { Check } from 'phosphor-react'
 import * as Checkbox from '@radix-ui/react-checkbox'
+import { FormEvent, useState } from 'react'
 
-const weekDays = [
+const availableWeekDays = [
   'Domingo',
   'Segunda-feira',
   'Terça-feira',
@@ -12,6 +13,22 @@ const weekDays = [
 ]
 
 export const NewHabitForm = () => {
+  const [title, setTitle] = useState('')
+  const [weekDays, setWeekDays] = useState<number[]>([])
+
+  const createNewHabit = (event: FormEvent) => {
+    event.preventDefault()
+    console.log('Creating new habit', weekDays, title)
+  }
+
+  const handleToogleWeekDay = (weekDay: number) => {
+    if (weekDays.includes(weekDay)) {
+      setWeekDays(weekDays.filter((day) => day !== weekDay))
+    } else {
+      setWeekDays([...weekDays, weekDay])
+    }
+  }
+
   return (
     <form className='w-full flex flex-col mt-6'>
       <label className='font-semibold leading-tight' htmlFor='title'>
@@ -23,26 +40,34 @@ export const NewHabitForm = () => {
         id='title'
         placeholder='Ex: Ler 30 minutos por dia'
         autoFocus
+        onChange={(event) => setTitle(event.target.value)}
       />
       <label className='font-semibold leading-tight mt-4' htmlFor='description'>
         Qual a recorrência
       </label>
-      <div
-        className='flex flex-col gap-2 mt-3
-      '
-      >
-        {weekDays.map((day) => (
-          <Checkbox.Root key={day} className='flex items-center gap-3 group'>
-            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 '>
-              <Checkbox.Indicator>
-                <Check size={20} className='text-white' />
-              </Checkbox.Indicator>
-            </div>
-            <span className='text-white leading-tight'>{day}</span>
-          </Checkbox.Root>
-        ))}
+      <div className='flex flex-col gap-2 mt-3'>
+        {availableWeekDays.map((weekDay, index) => {
+          return (
+            <Checkbox.Root
+              key={weekDay}
+              className='flex items-center gap-3 group'
+              onCheckedChange={() => handleToogleWeekDay(index)}
+            >
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500 transition-colors'>
+                <Checkbox.Indicator className=''>
+                  <Check size={20} className='text-white' />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className=' text-white leading-tight '>{weekDay}</span>
+            </Checkbox.Root>
+          )
+        })}
       </div>
-      <button className='mt-6 rounded-lg p4 gap-3 flex items-center font-semibold bg-green-600 justify-center hover:bg-green-500'>
+      <button
+        onClick={createNewHabit}
+        className='mt-6 rounded-lg p4 gap-3 flex items-center font-semibold bg-green-600 justify-center hover:bg-green-500'
+      >
         <Check size={20} weight='bold' />
         Confirmar
       </button>
