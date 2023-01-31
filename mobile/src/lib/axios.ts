855d07596f835b9542f9937ac11f437adba49308
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-01-31 23:33:15 +0100

    chore: add axios

--- mobile/src/lib/axios.ts
+++ mobile/src/lib/axios.ts
@@ -1,5 +1,13 @@
 import axios from 'axios'
+import { Platform } from 'react-native'
+
+let baseURL = ''
+if (Platform.OS === 'ios') {
+  baseURL = 'http://localhost:3333'
+} else {
+  baseURL = 'http://192.168.5.95:19000:3333'
+}
 
 export const api = axios.create({
-  baseURL: 'http://192.168.5.95:19000:3333'
+  baseURL: baseURL
 })
