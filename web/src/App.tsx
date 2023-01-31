615ff34e62765610f5ca6b3160908a7c62eba885
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-01-31 22:39:21 +0100

    chore: add toastify

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,20 @@
-// import Habit from './Habit'
+import './lib/dayjs'
+import './styles/global.css'
+import 'react-toastify/dist/ReactToastify.css'
 import { Header } from './components/Header'
 import { SummaryTable } from './components/SummaryTable'
-import './styles/global.css'
+import { ToastContainer } from 'react-toastify'
 
 export function App() {
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
-        <Header />
-        <SummaryTable />
+    <>
+      <ToastContainer theme='colored' autoClose={3000} />
+      <div className='w-screen h-screen flex justify-center items-center'>
+        <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+          <Header />
+          <SummaryTable />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
