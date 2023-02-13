c183ad27d4bfed82602ec34e4794091128043559
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-02-13 23:25:45 +0100

    feat: add push notifications with expo

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,10 +6,20 @@ import {
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter'
 
+import * as Notifications from 'expo-notifications'
+
 import { useFonts } from 'expo-font'
-import { StatusBar } from 'react-native'
+import { StatusBar, TouchableOpacity, Text } from 'react-native'
 import { Loading } from './src/components'
 import { Routes } from './src/routes'
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: true
+  })
+})
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,6 +30,27 @@ export default function App() {
   })
 
   if (!fontsLoaded) return <Loading />
+
+  async function getScheduledNotifications() {
+    const notifications =
+      await Notifications.getAllScheduledNotificationsAsync()
+    console.log(notifications)
+  }
+
+  async function scheduleNotification() {
+    const trigger = new Date(Date.now())
+    trigger.setMinutes(trigger.getHours() + 1)
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Hey, 🤗',
+        body: 'Está na hora de cuidar molhar suas plantinhas.'
+      },
+      trigger
+    })
+  }
+
+  scheduleNotification()
 
   return (
     <>
